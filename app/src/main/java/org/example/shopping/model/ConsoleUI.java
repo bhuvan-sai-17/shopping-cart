e3fcfa466bc732830a57934c278accca13128f21
bhu1Sai <kasukurthibhuvan@gmail.com>
2025-12-17 00:38:30 +0100

    GREEN: adding through ui adds product into cart

--- app/src/main/java/org/example/shopping/model/ConsoleUI.java
+++ app/src/main/java/org/example/shopping/model/ConsoleUI.java
@@ -6,14 +6,37 @@ import java.util.Scanner;
 
 public class ConsoleUI {
   private final CartService cartService;
-    private final PrintStream out;
-    private final Scanner scanner;
+  private final PrintStream out;
+  private final Scanner scanner;
 
-    public ConsoleUI(CartService cartService, PrintStream out, InputStream in) {
-        this.cartService = cartService;
-        this.out = out;
-        this.scanner = new Scanner(in);
+  public ConsoleUI(CartService cartService, PrintStream out, InputStream in) {
+    this.cartService = cartService;
+    this.out = out;
+    this.scanner = new Scanner(in);
+  }
+
+  public void start() {
+    
+    while (true) {
+      System.out.println("Enter either add or exit");
+      String command = scanner.nextLine();
+
+      if (command.equalsIgnoreCase("exit")) {
+        out.println("Bye!");
+        break;
+      }
+
+      if (command.equalsIgnoreCase("add")) {
+        String id = scanner.nextLine();
+        String name = scanner.nextLine();
+        double price = Double.parseDouble(scanner.nextLine());
+        int quantity = Integer.parseInt(scanner.nextLine());
+
+        Product product = new Product(id, name, price);
+        cartService.addProduct(product, quantity);
+      }
+
+      this.out.println("------");
     }
-
-    public void start() {}
+  }
 }
