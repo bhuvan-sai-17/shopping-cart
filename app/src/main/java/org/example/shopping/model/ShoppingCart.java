802ff6fffcff3828e6ead66c18682854413d06b8
bhu1Sai <kasukurthibhuvan@gmail.com>
2025-12-16 11:47:42 +0100

    GREEN: implemented shopping cart add product and calculate total of the cart

--- app/src/main/java/org/example/shopping/model/ShoppingCart.java
+++ app/src/main/java/org/example/shopping/model/ShoppingCart.java
@@ -2,15 +2,35 @@ package org.example.shopping.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 public class ShoppingCart {
 
+  private List<CartItem> items = new ArrayList<>();
 
   public ShoppingCart() {}
 
-  public void addProduct(Product product, int quantity) {}
+  public void addProduct(Product product, int quantity) {
+    for (CartItem item: items) {
+      if (item.getProduct().equals(product)) {
+        // increase quantity if the product is already a cart item
+        items.remove(item);
+        items.add(new CartItem(product, item.getQuantity() + quantity));
+        return;
+      }
+    }
+    items.add(new CartItem(product, quantity));
+  }
 
-  public List<CartItem> getItems() {return null ;}
+  public List<CartItem> getItems() {
+    return Collections.unmodifiableList(items);
+  }
 
-  public double getTotal() { return 0;}
+  public double getTotal() {
+    double total = 0;
+    for (CartItem item: items) {
+      total += item.getSubtotal();
+    }
+    return total;
+  }
 }
